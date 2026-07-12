7dcda20d527cb2886bf7f2f8080a0cc1e3acb5ce
Xevorius <tim.is@live.nl>
2026-07-12 21:55:53 +0000

    Bundle Barretenberg WASM for issuer decisions

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,11 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+  outputFileTracingIncludes: {
+    '/api/issuer/requests/\\[requestId\\]/decision': [
+      './node_modules/@aztec/bb.js/dest/node/barretenberg_wasm/barretenberg-threads.wasm.gz',
+    ],
+  },
   experimental: {
     optimizePackageImports: ['@union-networks/issuer', '@union-networks/web-login'],
   },
